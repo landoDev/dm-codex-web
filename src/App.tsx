3ce732d390f7e31b116e3dc863992d655a950b8c
Lando <landonlewisturner@gmail.com>
2023-11-13 14:59:02 -0500

    update to ts with cra

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <h1>Dungeon Master's Codex</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// fonts for material ui
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <App />
