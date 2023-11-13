2b036512524540ea0cc0fcc25fbaeab4319da280
Lando <landonlewisturner@gmail.com>
2023-11-13 17:31:13 -0500

    safety wip

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,17 +4,29 @@ import { Avatar, Stack } from '@mui/material'
 const NavContainer = styled.div`
     display: flex;
     background-color: black;
+    justify-content: space-between;
+    align-items: center;
 `
 const AppTitle = styled.h1`
     color: white;
+`
+const UserContainer = styled.div`
+    flex-direction: row;
+
+`
+const DisplayName = styled.p`
+    color: white;
+    text-align: center;
+    margin: auto;
 `
 
 const NavBar = () => {
     return (
         <NavContainer>
             <AppTitle>Dungeon Master's Codex</AppTitle>
-            <Stack>
-                <Avatar />
+            <Stack direction="row" spacing={2}>
+                <Avatar src=""/>
+                <DisplayName>Joe Freebird</DisplayName>
             </Stack>
         </NavContainer>
     )
