d236cb966617396245a837aa4d4b4ff843e7f4aa
Lando <landonlewisturner@gmail.com>
2023-11-14 14:17:09 -0500

    adding roll table component wip

---
+++ src/components/Table.tsx
@@ -0,0 +1,79 @@
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
+
+
+interface HeaderObject {
+    header: string;
+}
+
+interface RowObject {
+    headerKey: string;
+    content: string;
+}
+
+interface RollTableProps {
+    headers: HeaderObject[];
+    rows: RowObject[];
+}
+
+
+
+/**
+ * RollTable serves to render the dm tables like `Setting A DC` and random encounter tables.
+ * Takes the headers as names and row objects and assumes they are ordered as desired
+ * 
+ */
+const RollTable = ({headers, rows}) => {
+
+    return (
+        <TableContainer>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                <TableHead>
+                    <TableRow>
+                        <StyledTableCell>Dessert (100g serving)</StyledTableCell>
+                        <StyledTableCell align="right">Calories</StyledTableCell>
+                        <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
+                        <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
+                        <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                {rows.map((row) => (
+                    <StyledTableRow key={row.name}>
+                    <StyledTableCell component="th" scope="row">
+                        {row.name}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">{row.calories}</StyledTableCell>
+                    <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                    <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+                    <StyledTableCell align="right">{row.protein}</StyledTableCell>
+                    </StyledTableRow>
+                ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+}
---
+++ src/pages/session.tsx
@@ -0,0 +1,13 @@
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+const SessionPage = () => {
+    return <div>Placeholder</div>
+}
+
+export default SessionPage;
